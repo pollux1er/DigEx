c0dff53902882df65b610434c96d2e3ed143056b
Patient Assontia <assontia@gmail.com>
2018-03-31 07:59:11 +0100

    The initial website

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>DIGITAL EXPERIENCE - Coming Soon </title>
+    <meta name="description" content="DIGITAL EXPERIENCE - ">
+    <meta name="author" content="pixiefy">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,800%7CWork+Sans:500,900" rel="stylesheet">
+    
+    <!-- All CSS -->
+	<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link rel="stylesheet" href="css/style.css" />
+	<link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+	
+	<!-- Modernizr js - required -->
+	<script src="js/modernizr.custom.js"></script>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
+
+</head>
+<body id="mrs-ripples-canvas" class="mrs-v31">
+	
+	<!-- Page Loader Start -->
+	<div class="marshall-loading-screen">
+	    <div class="marshall-loading-icon">
+	        <div class="marshall-loading-inner">
+	        	<div class="marshall-load" data-name="DE"></div>
+	        </div>
+	    </div>
+	</div><!-- End .loading-screen -->
+	
+	<div class="marshall-container">
+		<div class="marshall-col-6 marshall-middle-6 marshall-col-content">
+			<div class="marshall-logo fadeIn fast">
+				<img src="images/logo-DIGEX-2.png" alt="Marshall Logo">
+			</div>
+			<div class="marshall-content jquery-center">
+				<h1 class="fadeIn medium futura-bold-style">We're launching our<br> <span>new Website</span></h1>
+				<p class="short_description fadeIn medium futura-style" style="font-size : 18px;">Working hard for something we don't care about is called stress. Working hard for smething we love is called passion.</p>
+				<div class="marshall-button-group fadeIn slow">
+					<div class="morph-button morph-button-modal morph-button-modal-2 marshall-morph-modal morph-button-fixed">
+						<button type="button" class="futura-style">Notify Us</button>
+						<div class="morph-content marshall-newsletter-form">
+							<div class="marshall-newsletter-inner">
+								<div class="content-style-form content-style-form-2 futura-style">
+									<div class="marshall-newsletter-header">
+										<div class="marshall-newsletter-animation-icons">
+											<span class="medium-icon animation-icon-1 animation-icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+											<span class="medium-icon animation-icon-2 animation-icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+											<span class="medium-icon animation-icon-3 animation-icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+											<span class="small-icon animation-icon-4 animation-icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+											<span class="small-icon animation-icon-5 animation-icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+											<span class="large-icon animation-icon-7 animation-icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+											<span class="large-icon animation-icon-8 animation-icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+											<span class="large-icon animation-icon-9 animation-icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+
+											<span class="medium-icon animation-icon-10 animation-icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+											<span class="large-icon animation-icon-16 animation-icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+											<span class="large-icon animation-icon-17 animation-icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+											<span class="large-icon animation-icon-18 animation-icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+										</div>
+										<img src="images/envelope.png" alt="">
+									</div>
+									<span class="close-newsletter icon-close"><i class="fa fa-times-thin fa-2x" aria-hidden="true"></i></span>
+									<div class="marshall-newsletter-description">
+										<h2>Sign Up</h2>
+										<p>Be first to know about the latest updates and get exclusive offer on our grand opening</p>
+									</div>
+									<form id="marshall-form" class="marshall-newsletter-content">
+										<input id="marshall-email" type="email" placeholder="Your Email">
+										<button class="marshall_submit" type="submit">Subscribe</button>
+										<label for="marshall-email"></label>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div><!-- morph-button -->
+				</div>
+			</div>
+		</div>
+		<div class="footer-copyright position_footer align-left">
+        	<p class="futura-style">&copy; DIGEX 2018, All right reserved</p>
+        </div>
+		<div class="stay_connent_with_social align-right">
+			<ul>
+				<!--<li><a href="https://twitter.com/pixiefy" class="tw_social"  title=""><i class="fa fa-twitter" aria-hidden="true"></i></a></li>-->
+				<li><a target="_blank" href="https://www.facebook.com/DigitalExperienceTech/" class="fb_social"  title=""><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+				<li><a target="_blank" href="https://www.linkedin.com/company/digital-experience-tech/" class="in_social"  title=""><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+				<!--<li><a href="https://plus.google.com/u/1/110725603752835072243" class="gp_social"  title=""><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>-->
+			</ul>
+		</div>
+
+	</div>
+
+	
+
+	<!-- All marshall js files -->
+	
+	<script src="js/classie.js"></script>
+	<script src="js/uiMorphingButton_fixed.js"></script>
+	<script src="js/form-init.js"></script>
+	<script src="js/jquery.ajaxchimp.min.js"></script>
+    <script type="text/javascript" src="js/v31/jquery.ripples-min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+	
+</body>
+</html>
